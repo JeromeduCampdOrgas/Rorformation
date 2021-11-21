8590aea2a566ee58a0b8275d26dee3f6a4242480
Jerome <ducampdorgasjerome@gmail.com>
2021-11-21 07:22:54 +0100

    finalisation CRUD posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,9 +13,29 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    post_params =params.require(:post).permit(:name, :content)
+    post_params = params.require(:post).permit(:name, :content)
     @post.update(post_params)
    redirect_to posts_path
   end
 
+  def  new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.create(post_params)
+    redirect_to post_path(post.id)
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:name, :content)
+  end
 end
